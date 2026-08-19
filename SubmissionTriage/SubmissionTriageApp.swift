a93522dbbd034eaa509c048c71f4530b8202dd79
Nino Amahan <ninochristian.amahan@gmail.com>
2026-08-19 09:04:44 +0800

    Initial commit

---
+++ SubmissionTriage/SubmissionTriageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SubmissionTriageApp.swift
+//  SubmissionTriage
+//
+//  Created by Niño Christian on 8/19/26.
+//
+
+import SwiftUI
+
+@main
+struct SubmissionTriageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
